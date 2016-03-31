0824834da840f89b319714e8b9f183f4a6116193
Caio Brentano dos Passos <caiobrentano@gmail.com>
2016-03-31 20:09:40 -0300

    tab module

--- app.js
+++ app.js
@@ -28,4 +28,17 @@
     },
   ];
 
+  app.controller('PanelController', function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab){
+      this.tab = setTab;
+    };
+
+    this.isSelected = function(checkTab) {
+        return checkTab === this.tab;
+    };
+
+  });
+
 })();
